f95096290f4de35274a3646aa8e162bd97c1084f
WinstonKamau <crispus.kamau@andela.com>
2018-08-06 11:36:28 +0300

    feat(apt): apt commands

---
+++ package_management/apt/apt.sh
@@ -0,0 +1,20 @@
+update_software() {
+    # view the sources list file
+    less /etc/apt/sources.list
+    # Get updates to your repository list and synchronize these updates to your cache so that when updating a pakage it installs directly from the cache
+    apt update
+    # Search through your updated cache list for a package
+    apt-cache search vlc
+    # Install the package
+    apt install vlc
+    # Remove package 
+    apt remove nginx
+    # Remove configuration files for the package that may have been left
+    apt --purge remove nginx
+    # Remove uneeded packages
+    apt autoremove
+    # Updates all packages based on the updated cache list
+    apt upgrade
+    # Updates all packages adds upgrades and removes unneeded packages
+    apt full-upgrade
+}
